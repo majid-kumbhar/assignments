36d8cdc38f8cd63aa437d7959a845b25442c88f2
MAJIDKUMBHAR <atvisback@gmail.com>
2023-11-24 15:57:23 +0500

    assignment 8 added

---
+++ assignment-8/script.js
@@ -0,0 +1,23 @@
+var questionsData = [
+{'Question': '1. What year did Pakistan gain independence?\n', 'Options': ['1947', '1956', '1935', '1965'], 'Answer': '1947'}, 
+{'Question': '2. The national poet of Pakistan is:\n', 'Options': ['Allama Iqbal', 'Faiz Ahmed Faiz', 'Mirza Ghalib', 'Ahmed Faraz'], 'Answer': 'Allama Iqbal'}, 
+{'Question': '3. The highest mountain in Pakistan is:\n', 'Options': ['Nanga Parbat', 'K2 (Godwin Austen', 'Gasherbrum I', 'Broad Peak'], 'Answer': 'K2 (Godwin Austen'},
+{'Question': '4. Which river is the longest in Pakistan?\n', 'Options': ['Indus', 'Jhelum', 'Ravi', 'Chenab'], 'Answer': 'Indus'}, 
+{'Question': '5. The founder of Pakistan, Muhammad Ali Jinnah, is also known as:\n', 'Options': ['Quaid e Azam', 'Shaheed e Millat', 'Bab e Pakistan', 'Sadiq e Pakistan'], 'Answer': 'Quaid e Azam'}, 
+{'Question': '6. The largest province by area in Pakistan is:\n', 'Options': ['Punjab', 'Sindh', 'Balochistan', 'Khyber Pakhtunkhwa'], 'Answer': 'Balochistan'}, 
+{'Question': '7. Pakistan shares its longest border with which country?\n', 'Options': ['China', 'India', 'Afghanistan', 'Iran'], 'Answer': 'India'}, 
+{'Question': '8. The national flower of Pakistan is:\n', 'Options': ['Jasmine', 'Rose', 'Sunflower', 'Tulip'], 'Answer': 'Jasmine'}, 
+{'Question': '9. Which city is known as the cultural capital of Pakistan?\n', 'Options': ['Karachi', 'Lahore', 'Islamabad', 'Peshawar'], 'Answer': 'Lahore'}, 
+{'Question': '10. The architectural landmark, Badshahi Mosque, is located in which city of Pakistan?\n', 'Options': ['Lahore', 'Karachi', 'Islamabad', 'Peshawar'], 'Answer': 'Lahore'}, 
+{'Question': "11. Pakistan's first television station was established in which year?\n", 'Options': ['1960', '1950', '1964', '1967'], 'Answer': '1960'}, 
+{'Question': '12. The traditional dress of Balochistan is known as:\n', 'Options': ['Shalwar Kameez', 'Lehenga Choli', 'Ajrak', 'Shalwar Kameez with waistcoat'], 'Answer': 'Shalwar Kameez with waistcoat'}, 
+{'Question': '13. The national animal of Pakistan is the:\n', 'Options': ['Markhor', 'Snow Leopard', 'Lion', 'Tiger'], 'Answer': 'Markhor'}, 
+{'Question': '14. Which mountain range is located in the northern areas of Pakistan?\n', 'Options': ['Himalayas', 'Karakoram', 'Andes', 'Alps'], 'Answer': 'Karakoram'}, 
+{'Question': '15. The Pakistan Resolution was passed in which year?\n', 'Options': ['1940', '1935', '1947', '1956'], 'Answer': '1940'}, 
+{'Question': '16. The largest dam in Pakistan is:\n', 'Options': ['Tarbela Dam', 'Mangla Dam', 'Warsak Dam', 'Ghazi Barotha Dam'], 'Answer': 'Tarbela Dam'}, 
+{'Question': '17. Who is known as the "Father of the Nation" in Pakistan?\n', 'Options': ['Allama Iqbal', 'Muhammad Ali Jinnah', 'Liaquat Ali Khan', 'Sir Syed Ahmed Khan'], 'Answer': 'Muhammad Ali Jinnah'}, 
+{'Question': '18. The currency of Pakistan is called:\n', 'Options': ['Rupee', 'Taka', 'Rial', 'Dinar'], 'Answer': 'Rupee'}, 
+{'Question': '19. Which pass connects Pakistan with China?\n', 'Options': ['Bolan Pass', 'Khyber Pass', 'Khunjerab Pass', 'Tochi Pass'], 'Answer': 'Khunjerab Pass'}, 
+{'Question': '20. The first Prime Minister of Pakistan was:\n', 'Options': ['Liaquat Ali Khan', 'Zulfikar Ali Bhutto', 'Nawaz Sharif', 'Ayub Khan'], 'Answer': 'Liaquat Ali Khan'}]
+
+
